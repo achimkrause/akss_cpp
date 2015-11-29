159c75aca0626d6b7d85ded269148ac63468e999
Fabian Henneke <fabian.henneke@gmail.com>
2015-11-29 01:02:46 +0100

    Implementation of a generic interactive shell

---
+++ src/interactive_shell.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+class ShellCommand
+{
+ public:
+  virtual ~ShellCommand();
+  virtual bool execute(const std::vector<std::string>& args) = 0;
+};
+
+using CommandMap = std::map<std::string, std::unique_ptr<ShellCommand>>;
+
+class InteractiveShell
+{
+ public:
+  void register_command(std::string name,
+                        std::unique_ptr<ShellCommand> command);
+  void run();
+
+ private:
+  void execute(std::string name, const std::vector<std::string>& args);
+
+  static std::string PROMPT_;
+  CommandMap commands_;
+};
--- src/spectral_sequence.cpp
+++ src/spectral_sequence.cpp
@@ -2,8 +2,7 @@
 
 #include <tuple>
 
-TrigradedIndex::TrigradedIndex(const int p, const int q,
-                             const int s)
+TrigradedIndex::TrigradedIndex(const int p, const int q, const int s)
     : p_(p), q_(q), s_(s)
 {
 }
@@ -21,109 +20,124 @@ bool operator<(const TrigradedIndex& a, const TrigradedIndex& b)
   return std::tie(a_deg, a.p_, a.s_) < std::tie(b_deg, b.p_, b.s_);
 }
 
-TrigradedIndex operator+(const TrigradedIndex&a, const TrigradedIndex& b) {
-	return TrigradedIndex(a.p_ + b.p_, a.q_ + b.q_, a.s_ + b.s_);
-}
-
-
-GroupSequence::GroupSequence(const std::size_t index_min, const AbelianGroup& grp)
-	: done_(false), current_(index_min)
+TrigradedIndex operator+(const TrigradedIndex& a, const TrigradedIndex& b)
 {
-	entries_.emplace(index_min,std::make_tuple(grp, MatrixQ::identity(grp.rank())));
+  return TrigradedIndex(a.p_ + b.p_, a.q_ + b.q_, a.s_ + b.s_);
 }
 
-void GroupSequence::append(const std::size_t index, const AbelianGroup& grp, const MatrixQ& map){
-	if(index <= current_) {
-		throw std::logic_error("GroupSequence::append: Index is already set");
-	}
-
-	entries_.emplace(index, std::make_tuple(grp,map));
-	current_ = index;
+GroupSequence::GroupSequence(const std::size_t index_min,
+                             const AbelianGroup& grp)
+    : done_(false), current_(index_min)
+{
+  entries_.emplace(index_min,
+                   std::make_tuple(grp, MatrixQ::identity(grp.rank())));
 }
 
-void GroupSequence::done() {
-	done_ = true;
+void GroupSequence::append(const std::size_t index, const AbelianGroup& grp,
+                           const MatrixQ& map)
+{
+  if (index <= current_) {
+    throw std::logic_error("GroupSequence::append: Index is already set");
+  }
+
+  entries_.emplace(index, std::make_tuple(grp, map));
+  current_ = index;
 }
 
-const AbelianGroup& GroupSequence::get_group(const std::size_t index){
-	if(index < entries_.begin()->first) {
-		throw std::logic_error("GroupSequence::get_group: Index is less than min_index");
-	}
-	if(!done_ && index > current_) {
-		throw std::logic_error("GroupSequence::get_group: Index is not yet set");
-	}
-	std::map<std::size_t, std::tuple<AbelianGroup,MatrixQ>>::iterator
-	    pos = entries_.upper_bound(index);
-
-	return(std::get<0>(pos->second));
+void GroupSequence::done()
+{
+  done_ = true;
 }
 
-const MatrixQ& GroupSequence::get_matrix(const std::size_t index){
-	if(index < entries_.begin()->first) {
-		throw std::logic_error("GroupSequence::get_matrix: Index is less than min_index");
-	}
-	if(!done_ && index > current_) {
-		throw std::logic_error("GroupSequence::get_matrix: Index is not yet set");
-	}
-	std::map<std::size_t, std::tuple<AbelianGroup,MatrixQ>>::iterator
-	    pos = entries_.upper_bound(index);
+const AbelianGroup& GroupSequence::get_group(const std::size_t index)
+{
+  if (index < entries_.begin()->first) {
+    throw std::logic_error(
+        "GroupSequence::get_group: Index is less than min_index");
+  }
+  if (!done_ && index > current_) {
+    throw std::logic_error("GroupSequence::get_group: Index is not yet set");
+  }
+  std::map<std::size_t, std::tuple<AbelianGroup, MatrixQ>>::iterator pos =
+      entries_.upper_bound(index);
 
-	return(std::get<1>(pos->second));
+  return (std::get<0>(pos->second));
 }
 
-std::size_t GroupSequence::get_current(){
-	return current_;
+const MatrixQ& GroupSequence::get_matrix(const std::size_t index)
+{
+  if (index < entries_.begin()->first) {
+    throw std::logic_error(
+        "GroupSequence::get_matrix: Index is less than min_index");
+  }
+  if (!done_ && index > current_) {
+    throw std::logic_error("GroupSequence::get_matrix: Index is not yet set");
+  }
+  std::map<std::size_t, std::tuple<AbelianGroup, MatrixQ>>::iterator pos =
+      entries_.upper_bound(index);
+
+  return (std::get<1>(pos->second));
 }
 
-void GroupSequence::inc(){
-	++current_;
+std::size_t GroupSequence::get_current()
+{
+  return current_;
 }
 
-
-void SpectralSequence::set_diff(TrigradedIndex pqs, std::size_t r, MatrixQ matrix){
-	TrigradedIndex diff_offset(-r,r-1,1); //is that cast okay?
-
-
-	std::map<TrigradedIndex,GroupSequence>::iterator
-	           cokers = cokernels_.find(pqs);
-	std::map<TrigradedIndex,GroupSequence>::iterator
-		       kers = cokernels_.find(pqs+diff_offset);
-	if(kers == kernels_.end()) {
-		throw std::logic_error("SpectralSequence::set_diff: Kernel is not set.");
-	}
-	if(cokers == cokernels_.end()) {
-		throw std::logic_error("SpectralSequence::set_diff: Cokernel is not set.");
-	}
-	if(kers -> second . get_current() != r) {
-		throw std::logic_error("SpectralSequence::set_diff: Kernel is at wrong r.");
-	}
-	if(cokers -> second . get_current() != r) {
-		throw std::logic_error("SpectralSequence::set_diff: Cokernel is at wrong r.");
-	}
-
-	AbelianGroup X = kers->second.get_group(r);
-	AbelianGroup Y = cokers->second.get_group(r);
-
-	if(morphism_zero(prime_,matrix, Y)){
-		kers->second.inc();
-		cokers->second.inc();
-		return;
-	}
-
-	MatrixQ inc_X = kers->second.get_matrix(r);
-	MatrixQ proj_Y = cokers->second.get_matrix(r);
-
-	MatrixQList from_X, to_Y;
-	from_X.emplace_back(inc_X);
-	to_Y.emplace_back(proj_Y);
-
-	GroupWithMorphisms new_kernel = compute_kernel(prime_, matrix, X, Y, MatrixQRefList(), ref(from_X));
-	GroupWithMorphisms new_cokernel = compute_cokernel(prime_, matrix, Y, ref(to_Y),MatrixQRefList());
-
-	kers->second.append(r+1,new_kernel.group, new_kernel.maps_from[0]);
-	cokers->second.append(r+1,new_cokernel.group, new_cokernel.maps_to[0]);
+void GroupSequence::inc()
+{
+  ++current_;
 }
 
-const AbelianGroup& SpectralSequence::get_e_ab(TrigradedIndex pqs, std::size_t a, std::size_t b) {
+void SpectralSequence::set_diff(TrigradedIndex pqs, std::size_t r,
+                                MatrixQ matrix)
+{
+  TrigradedIndex diff_offset(-r, r - 1, 1);  // is that cast okay?
 
+  std::map<TrigradedIndex, GroupSequence>::iterator cokers =
+      cokernels_.find(pqs);
+  std::map<TrigradedIndex, GroupSequence>::iterator kers =
+      cokernels_.find(pqs + diff_offset);
+  if (kers == kernels_.end()) {
+    throw std::logic_error("SpectralSequence::set_diff: Kernel is not set.");
+  }
+  if (cokers == cokernels_.end()) {
+    throw std::logic_error("SpectralSequence::set_diff: Cokernel is not set.");
+  }
+  if (kers->second.get_current() != r) {
+    throw std::logic_error("SpectralSequence::set_diff: Kernel is at wrong r.");
+  }
+  if (cokers->second.get_current() != r) {
+    throw std::logic_error(
+        "SpectralSequence::set_diff: Cokernel is at wrong r.");
+  }
+
+  AbelianGroup X = kers->second.get_group(r);
+  AbelianGroup Y = cokers->second.get_group(r);
+
+  if (morphism_zero(prime_, matrix, Y)) {
+    kers->second.inc();
+    cokers->second.inc();
+    return;
+  }
+
+  MatrixQ inc_X = kers->second.get_matrix(r);
+  MatrixQ proj_Y = cokers->second.get_matrix(r);
+
+  MatrixQList from_X, to_Y;
+  from_X.emplace_back(inc_X);
+  to_Y.emplace_back(proj_Y);
+
+  GroupWithMorphisms new_kernel =
+      compute_kernel(prime_, matrix, X, Y, MatrixQRefList(), ref(from_X));
+  GroupWithMorphisms new_cokernel =
+      compute_cokernel(prime_, matrix, Y, ref(to_Y), MatrixQRefList());
+
+  kers->second.append(r + 1, new_kernel.group, new_kernel.maps_from[0]);
+  cokers->second.append(r + 1, new_cokernel.group, new_cokernel.maps_to[0]);
+}
+
+const AbelianGroup& SpectralSequence::get_e_ab(TrigradedIndex pqs,
+                                               std::size_t a, std::size_t b)
+{
 }
