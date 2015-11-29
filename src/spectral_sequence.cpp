8b8fb616287f2fb5f29f031946af89b36ba5b716
Fabian Henneke <fabian.henneke@gmail.com>
2015-11-29 01:31:06 +0100

    Inserted a finite number of <P>s

--- src/spectral_sequence.cpp
+++ src/spectral_sequence.cpp
@@ -25,16 +25,18 @@ TrigradedIndex operator+(const TrigradedIndex& a, const TrigradedIndex& b)
   return TrigradedIndex(a.p_ + b.p_, a.q_ + b.q_, a.s_ + b.s_);
 }
 
-GroupSequence::GroupSequence(const std::size_t index_min,
-                             const AbelianGroup& grp)
+template <unsigned int P>
+GroupSequence<P>::GroupSequence(const std::size_t index_min,
+                                const AbelianGroup<P>& grp)
     : done_(false), current_(index_min)
 {
   entries_.emplace(index_min,
                    std::make_tuple(grp, MatrixQ::identity(grp.rank())));
 }
 
-void GroupSequence::append(const std::size_t index, const AbelianGroup& grp,
-                           const MatrixQ& map)
+template <unsigned int P>
+void GroupSequence<P>::append(const std::size_t index,
+                              const AbelianGroup<P>& grp, const MatrixQ& map)
 {
   if (index <= current_) {
     throw std::logic_error("GroupSequence::append: Index is already set");
@@ -49,7 +51,8 @@ void GroupSequence::done()
   done_ = true;
 }
 
-const AbelianGroup& GroupSequence::get_group(const std::size_t index)
+template <unsigned int P>
+const AbelianGroup<P>& GroupSequence<P>::get_group(const std::size_t index)
 {
   if (index < entries_.begin()->first) {
     throw std::logic_error(
@@ -58,13 +61,13 @@ const AbelianGroup& GroupSequence::get_group(const std::size_t index)
   if (!done_ && index > current_) {
     throw std::logic_error("GroupSequence::get_group: Index is not yet set");
   }
-  std::map<std::size_t, std::tuple<AbelianGroup, MatrixQ>>::iterator pos =
-      entries_.upper_bound(index);
+  auto pos = entries_.upper_bound(index);
 
   return (std::get<0>(pos->second));
 }
 
-const MatrixQ& GroupSequence::get_matrix(const std::size_t index)
+template <unsigned int P>
+const MatrixQ& GroupSequence<P>::get_matrix(const std::size_t index)
 {
   if (index < entries_.begin()->first) {
     throw std::logic_error(
@@ -73,30 +76,31 @@ const MatrixQ& GroupSequence::get_matrix(const std::size_t index)
   if (!done_ && index > current_) {
     throw std::logic_error("GroupSequence::get_matrix: Index is not yet set");
   }
-  std::map<std::size_t, std::tuple<AbelianGroup, MatrixQ>>::iterator pos =
-      entries_.upper_bound(index);
+  auto pos = entries_.upper_bound(index);
 
   return (std::get<1>(pos->second));
 }
 
-std::size_t GroupSequence::get_current()
+template <unsigned int P>
+std::size_t GroupSequence<P>::get_current()
 {
   return current_;
 }
 
-void GroupSequence::inc()
+template <unsigned int P>
+void GroupSequence<P>::inc()
 {
   ++current_;
 }
 
-void SpectralSequence::set_diff(TrigradedIndex pqs, std::size_t r,
-                                MatrixQ matrix)
+template <unsigned int P>
+void SpectralSequence<P>::set_diff(TrigradedIndex pqs, std::size_t r,
+                                   MatrixQ matrix)
 {
   TrigradedIndex diff_offset(-r, r - 1, 1);  // is that cast okay?
 
-  std::map<TrigradedIndex, GroupSequence>::iterator kers = kernels_.find(pqs);
-  std::map<TrigradedIndex, GroupSequence>::iterator cokers =
-      cokernels_.find(pqs + diff_offset);
+  auto kers = kernels_.find(pqs);
+  auto cokers = cokernels_.find(pqs + diff_offset);
   if (kers == kernels_.end()) {
     throw std::logic_error("SpectralSequence::set_diff: Kernel is not set.");
   }
@@ -111,8 +115,8 @@ void SpectralSequence::set_diff(TrigradedIndex pqs, std::size_t r,
         "SpectralSequence::set_diff: Cokernel is at wrong r.");
   }
 
-  AbelianGroup X = kers->second.get_group(r);
-  AbelianGroup Y = cokers->second.get_group(r);
+  AbelianGroup<P> X = kers->second.get_group(r);
+  AbelianGroup<P> Y = cokers->second.get_group(r);
 
   if (morphism_zero(prime_, matrix, Y)) {
     kers->second.inc();
@@ -127,9 +131,9 @@ void SpectralSequence::set_diff(TrigradedIndex pqs, std::size_t r,
   from_X.emplace_back(inc_X);
   to_Y.emplace_back(proj_Y);
 
-  GroupWithMorphisms new_kernel =
+  GroupWithMorphisms<P> new_kernel =
       compute_kernel(prime_, matrix, X, Y, MatrixQRefList(), ref(from_X));
-  GroupWithMorphisms new_cokernel =
+  GroupWithMorphisms<P> new_cokernel =
       compute_cokernel(prime_, matrix, Y, ref(to_Y), MatrixQRefList());
 
   kers->second.append(r + 1, new_kernel.group, new_kernel.maps_from[0]);
@@ -139,12 +143,12 @@ void SpectralSequence::set_diff(TrigradedIndex pqs, std::size_t r,
 // computes the kernel of the differentials up to d_{a-1} mod the image of the
 // differentials up to d_{b-1}.
 // for example, get_e_ab(pqs, r, r) computes the E_r page at pqs.
-const AbelianGroup SpectralSequence::get_e_ab(TrigradedIndex pqs, std::size_t a,
+template <unsigned int P>
+const AbelianGroup<P> SpectralSequence<P>::get_e_ab(TrigradedIndex pqs, std::size_t a,
                                               std::size_t b)
 {
-  std::map<TrigradedIndex, GroupSequence>::iterator kers = kernels_.find(pqs);
-  std::map<TrigradedIndex, GroupSequence>::iterator cokers =
-      cokernels_.find(pqs);
+  auto kers = kernels_.find(pqs);
+  auto cokers = cokernels_.find(pqs);
   if (kers == kernels_.end()) {
     throw std::logic_error("SpectralSequence::get_e_ab: Kernel is not set.");
   }
@@ -159,11 +163,11 @@ const AbelianGroup SpectralSequence::get_e_ab(TrigradedIndex pqs, std::size_t a,
         "SpectralSequence::get_e_ab: Cokernel is at wrong r.");
   }
 
-  AbelianGroup K = kers->second.get_group(a);
-  AbelianGroup C = cokers->second.get_group(b);
+  AbelianGroup<P> K = kers->second.get_group(a);
+  AbelianGroup<P> C = cokers->second.get_group(b);
 
   MatrixQ map = (cokers->second.get_matrix(b)) * (kers->second.get_matrix(a));
 
-  GroupWithMorphisms I = compute_image(prime_, map, K, C);
+  GroupWithMorphisms<P> I = compute_image(prime_, map, K, C);
   return I.group;
 }
--- src/spectral_sequence.h
+++ src/spectral_sequence.h
@@ -25,7 +25,9 @@ class TrigradedIndex
 
   friend bool operator==(const TrigradedIndex& a, const TrigradedIndex& b);
   friend bool operator<(const TrigradedIndex& a, const TrigradedIndex& b);
-  friend TrigradedIndex operator+(const TrigradedIndex&a, const TrigradedIndex& b);
+  friend TrigradedIndex operator+(const TrigradedIndex& a,
+                                  const TrigradedIndex& b);
+
  private:
   const int p_;
   const int q_;
@@ -34,39 +36,47 @@ class TrigradedIndex
 
 bool operator==(const TrigradedIndex& a, const TrigradedIndex& b);
 bool operator<(const TrigradedIndex& a, const TrigradedIndex& b);
-TrigradedIndex operator+(const TrigradedIndex&a, const TrigradedIndex& b);
+TrigradedIndex operator+(const TrigradedIndex& a, const TrigradedIndex& b);
 
-class GroupSequence {
+template<unsigned int P>
+class GroupSequence
+{
  public:
-	GroupSequence(const std::size_t index_min, const AbelianGroup& grp);
-	const AbelianGroup& get_group(const std::size_t index);
-	const MatrixQ& get_matrix(const std::size_t index);
-	void append(const std::size_t index, const AbelianGroup& grp, const MatrixQ& map);
-	void done();
-	std::size_t get_current();
-	void inc();
+  GroupSequence(const std::size_t index_min, const AbelianGroup<P>& grp);
+  const AbelianGroup<P>& get_group(const std::size_t index);
+  const MatrixQ& get_matrix(const std::size_t index);
+  void append(const std::size_t index, const AbelianGroup<P>& grp,
+              const MatrixQ& map);
+  void done();
+  std::size_t get_current();
+  void inc();
 
  private:
-	bool done_;
-	std::map<std::size_t, std::tuple<AbelianGroup,MatrixQ>> entries_;
-	std::size_t current_;
+  bool done_;
+  std::map<std::size_t, std::tuple<AbelianGroup<P>, MatrixQ>> entries_;
+  std::size_t current_;
 
-	//The matrix nr n represents the map between the group nr index_min and the n-th group.
-	//if number n is not set explicitly, but smaller than current,
-	//its value is given by the next smaller index.
-	//if done is set, arbitrarily large indices are allowed,
-	//and all higher things are treated as equal to the highest one that has been set explicitly.
+  // The matrix nr n represents the map between the group nr index_min and the
+  // n-th group.
+  // if number n is not set explicitly, but smaller than current,
+  // its value is given by the next smaller index.
+  // if done is set, arbitrarily large indices are allowed,
+  // and all higher things are treated as equal to the highest one that has been
+  // set explicitly.
 };
 
-class SpectralSequence {
+template<unsigned int P>
+class SpectralSequence
+{
+ public:
+  void set_diff(TrigradedIndex pqs, std::size_t r, MatrixQ matrix);
+  const AbelianGroup<P> get_e_ab(TrigradedIndex pqs, std::size_t a, std::size_t b);
 
-public:
-	void set_diff(TrigradedIndex pqs, std::size_t r, MatrixQ matrix);
-	const AbelianGroup get_e_ab(TrigradedIndex pqs, std::size_t a, std::size_t b);
-private:
-	std::map<TrigradedIndex, GroupSequence> kernels_;
-	std::map<TrigradedIndex, GroupSequence> cokernels_;
-	std::map<TrigradedIndex, MatrixQList> differentials_;
-	//const TrigradedIndex diff_offset_; oops, depends on r. Do we want a function object for that?
-	std::size_t prime_;
+ private:
+  std::map<TrigradedIndex, GroupSequence<P>> kernels_;
+  std::map<TrigradedIndex, GroupSequence<P>> cokernels_;
+  std::map<TrigradedIndex, MatrixQList> differentials_;
+  // const TrigradedIndex diff_offset_; oops, depends on r. Do we want a
+  // function object for that?
+  std::size_t prime_;
 };
